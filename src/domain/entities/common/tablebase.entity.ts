527a8b0b6ec80f113fffc8bbac51f93134cbc1bd
Roberto Suárez <electrosonix12@gmail.com>
2025-02-15 00:27:29 -0500

    fechas tipos timestamptz

--- src/domain/entities/common/tablebase.entity.ts
+++ src/domain/entities/common/tablebase.entity.ts
@@ -1,17 +1,33 @@
-import { CreateDateColumn, DeleteDateColumn, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-
+import {
+    CreateDateColumn,
+    DeleteDateColumn,
+    PrimaryColumn,
+    UpdateDateColumn
+} from "typeorm";
 
 export class TableBase {
 
     @PrimaryColumn({ default: () => 'gen_random_uuid()' })
     uid: string;
 
-    @CreateDateColumn({ name: 'created_at' })
+    @CreateDateColumn({
+        name: 'created_at',
+        type: 'timestamptz',
+        default: () => "CURRENT_TIMESTAMP"
+    })
     createdAt: Date;
 
-    @UpdateDateColumn({ name: 'updated_at' })
+    @UpdateDateColumn({
+        name: 'updated_at',
+        type: 'timestamptz',
+        default: () => "CURRENT_TIMESTAMP"
+    })
     updatedAt: Date;
 
-    @DeleteDateColumn({ name: 'deleted_at' })
+    @DeleteDateColumn({
+        name: 'deleted_at',
+        type: 'timestamptz',
+        nullable: true  // Se recomienda que sea nullable si aún no se ha eliminado el registro
+    })
     deletedAt: Date;
 }
